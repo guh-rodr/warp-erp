9ddceab9e3aaa52500290e32d5806141d2f95210
Gustavo Rodrigues <dev.gustavor@outlook.com>
2026-02-24 16:37:28 -0300

    feat: expandable sidebar

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 import { Sidebar } from './Sidebar';
 
 interface Props {
@@ -7,15 +7,26 @@ interface Props {
 }
 
 export function DashboardLayout({ title, children }: Props) {
-  return (
-    <div>
-      <Sidebar />
+  const getSidebarState = () => {
+    return localStorage.getItem('sidebarExpanded') === 'true';
+  };
 
-      <div style={{ marginLeft: 290 }} className="page-content p-20 space-y-10">
+  const handleSetExpanded = (value: boolean) => {
+    localStorage.setItem('sidebarExpanded', String(value));
+    setIsSidebarExpanded(value);
+  };
+
+  const [isSidebarExpanded, setIsSidebarExpanded] = useState(getSidebarState);
+
+  return (
+    <div className="w-full">
+      <Sidebar isExpanded={isSidebarExpanded} setIsExpanded={handleSetExpanded} />
+
+      <main className={`${isSidebarExpanded ? 'ml-72' : 'ml-18'} p-12 space-y-10 transition-all duration-300`}>
         <h1 className="font-semibold text-4xl">{title}</h1>
 
         <div className="space-y-5">{children}</div>
-      </div>
+      </main>
     </div>
   );
 }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { HouseIcon, IconProps, SignOutIcon } from '@phosphor-icons/react';
+import { ArrowsHorizontalIcon, HouseIcon, IconProps, SignOutIcon } from '@phosphor-icons/react';
 import { NavLink } from 'react-router';
 import { useAuth } from '../contexts/session/auth-context';
 import { MODULES } from '../utils/modules';
@@ -18,34 +18,61 @@ const LINKS: Link[] = [
   ...MODULES,
 ];
 
-export function Sidebar() {
+interface Props {
+  isExpanded: boolean;
+  setIsExpanded: (value: boolean) => void;
+}
+
+export function Sidebar({ isExpanded, setIsExpanded }: Props) {
   const { signOut } = useAuth();
 
   return (
-    <aside className="min-w-[290px] fixed max-h-screen h-full flex flex-col bg-white border-r border-neutral-200 p-6 shadow-sm">
+    <aside
+      className={`${isExpanded ? 'w-72' : 'w-18'} fixed max-h-screen h-full flex flex-col bg-white border-r border-neutral-200 shadow-sm transition-all duration-300`}
+    >
       <ul className="mt-8 text-neutral-600 text-[15px] flex flex-col h-full">
+        <li className="pl-6 pb-6">
+          <button
+            type="button"
+            onClick={() => setIsExpanded(!isExpanded)}
+            className="flex w-full gap-4 truncate hover:text-black"
+          >
+            <ArrowsHorizontalIcon {...iconProps} className="min-w-[20px]" size={20} />
+
+            <span className={`${isExpanded ? 'opacity-100' : 'opacity-0'} transition-all duration-300`}>
+              Ocultar barra
+            </span>
+          </button>
+        </li>
+
         {LINKS.map(({ icon: Icon, ...link }) => (
           <li key={link.key}>
             <NavLink
               to={link.href}
               className={({ isActive }) =>
-                `flex items-center gap-4 py-2.5 transition-colors ${isActive && link.key !== 'home' ? 'text-emerald-500' : 'hover:text-black'}`
+                `flex pl-6 gap-4 py-3 transition-colors ${isActive && link.key !== 'home' ? 'text-emerald-500' : 'hover:text-black'} truncate`
               }
             >
-              <Icon {...iconProps} />
-              {link.name}
+              <Icon {...iconProps} className="min-w-[20px]" size={20} />
+
+              <span className={`${isExpanded ? 'opacity-100' : 'opacity-0'} transition-all duration-300`}>
+                {link.name}
+              </span>
             </NavLink>
           </li>
         ))}
 
-        <li className="mt-auto">
+        <li className="pl-6 pb-6 mt-auto">
           <button
             type="button"
             onClick={signOut}
-            className="flex w-full items-center gap-4 text-red-500 hover:text-red-700 transition-colors"
+            className="flex w-full gap-4 truncate text-red-500 hover:text-red-700"
           >
-            <SignOutIcon {...iconProps} />
-            Desconectar
+            <SignOutIcon {...iconProps} className="min-w-[20px]" size={20} />
+
+            <span className={`${isExpanded ? 'opacity-100' : 'opacity-0'} transition-all duration-300`}>
+              Desconectar
+            </span>
           </button>
         </li>
       </ul>
