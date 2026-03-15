4756e4913819c427d90d0ba1d557724b7303cb9c
Gustavo Rodrigues <dev.gustavor@outlook.com>
2026-03-15 00:54:43 -0300

    feat: remove models fields from category creation form

--- src/types/category.ts
+++ src/types/category.ts
@@ -1,14 +1,10 @@
-import { ModelItem, ModelItemForm } from './model';
-
 export interface CategoryItem {
   id: string;
   name: string;
-  models: ModelItem[];
 }
 
 export interface CreateCategoryForm {
   name: string;
-  models: ModelItemForm[];
 }
 
 export type EditCategoryForm = Pick<CategoryItem, 'id' | 'name'>;
