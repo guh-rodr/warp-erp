e13de0493a303c342c8aed428ab012f4cb43b39c
Gustavo Rodrigues <dev.gustavor@outlook.com>
2026-03-27 19:08:25 -0300

    chore: eslint rule to ignore unused variables starting with underline

--- eslint.config.ts
+++ eslint.config.ts
@@ -28,6 +28,14 @@ export default tseslint.config(
       ...reactHooks.configs.recommended.rules,
 
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
     },
   },
 );
