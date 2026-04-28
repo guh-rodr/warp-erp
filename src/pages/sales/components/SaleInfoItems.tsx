972e4d42f64f141cd2a8b20d365817e269c44a59
Gustavo Rodrigues <dev.gustavor@outlook.com>
2026-04-28 14:37:40 -0300

    refactor: update SaleInfoItems card design and support for variants

--- src/pages/sales/components/SaleInfoItems.tsx
+++ src/pages/sales/components/SaleInfoItems.tsx
@@ -7,7 +7,6 @@ import { formatToReal } from '../../../functions/currency';
 import { useFetchSaleItems } from '../../../hooks/useSales';
 import { SaleRow } from '../../../types/sale';
 import { COLORS } from '../../../utils/colors';
-import { PRINTS } from '../../../utils/prints';
 
 interface Props {
   id: SaleRow['id'];
@@ -26,6 +25,8 @@ export function SaleInfoItems({ id }: Props) {
     }
   };
 
+  const getPercentage = (n1: number, n2: number) => Math.floor((n1 / n2) * 100);
+
   return (
     <div className="mt-4 space-y-1">
       {isLoading && <LoadingNotification />}
@@ -35,61 +36,92 @@ export function SaleInfoItems({ id }: Props) {
       {isSuccess && !!data && (
         <div className="space-y-6">
           {data.map((item) => {
-            const print = PRINTS.find((p) => p.value === item.print);
-            const color = COLORS.find((c) => c.value === item.color);
+            const color = COLORS.find((c) => c.value === item.variant.color);
+            const profit = item.salePrice - item.costPrice;
 
             return (
               <Card key={item.id} className="p-0">
                 <div
-                  className="flex text-sm p-2.5 hover:bg-neutral-100/70 transition-colors cursor-pointer"
+                  className="flex gap-1 text-sm p-2.5 hover:bg-neutral-100/70 transition-colors cursor-pointer"
                   onClick={() => handleExpandSale(item.id!)}
                 >
-                  <span className="bg-gray-200/20 border border-gray-200 rounded-lg p-2 block w-fit text-gray-300">
+                  <span className="bg-gray-200/20 border border-gray-200 rounded-lg p-2 block size-fit text-gray-300 my-auto">
                     <TShirtIcon size={24} />
                   </span>
 
                   <div className="flex items-center flex-1 justify-between">
                     <div className="flex flex-col justify-between ml-2">
-                      <span className="pt-0.5">
-                        <strong className="font-semibold">{item.categoryName}</strong> - {item.modelName}
-                      </span>
+                      <div className="space-x-2">
+                        <span className="rounded-full text-center text-xs w-fit px-2 border border-indigo-100 font-medium bg-indigo-100/50 text-indigo-500">
+                          {item.categoryName}
+                        </span>
 
-                      <div className="text-neutral-500">{formatToReal(item.salePrice)}</div>
+                        <span className="font-medium">{item.modelName}</span>
+                      </div>
+
+                      <span className="pt-0.5 text-[15px] font-medium">
+                        {item.variant.color && color && item.variant.size && (
+                          <span className="text-sm font-normal text-neutral-500 pl-1">
+                            {item.variant.size.toUpperCase()} ∙ {color.label}
+                          </span>
+                        )}
+                      </span>
                     </div>
 
-                    <CaretRightIcon
-                      weight="bold"
-                      size={14}
-                      className={`text-neutral-500 ${expandedItemId === item.id ? 'rotate-90' : ''} transition-all`}
-                    />
+                    <div className="flex items-center gap-4">
+                      <div>
+                        <span className="font-medium text-[15px] block">
+                          {formatToReal(item.quantity * item.salePrice)}
+                        </span>
+                        <span className="text-sm text-neutral-500">
+                          {item.quantity} {item.quantity > 1 ? 'itens' : 'item'}
+                        </span>
+                      </div>
+                      <CaretRightIcon
+                        weight="bold"
+                        size={14}
+                        className={`text-neutral-500 ${expandedItemId === item.id ? 'rotate-90' : ''} transition-all`}
+                      />
+                    </div>
                   </div>
                 </div>
                 {expandedItemId === item.id && (
                   <div className="p-2.5 pt-0">
                     <div className="grid grid-cols-3 gap-y-3 w-full border-t border-neutral-200 py-2 px-1">
-                      <div className="flex flex-col text-sm text-neutral-500">
-                        Tamanho
-                        <span className="text-black">{item.size?.toUpperCase() || 'N/A'}</span>
+                      {item.variant.color && color && item.variant.size && (
+                        <>
+                          <div className="flex flex-col text-sm text-neutral-800 font-medium">
+                            Tamanho
+                            <span className="text-neutral-500 font-normal">{item.variant.size.toUpperCase()}</span>
+                          </div>
+
+                          <div className="flex flex-col text-sm text-neutral-800 font-medium">
+                            Cor
+                            <span className="text-neutral-500 font-normal">{color.label}</span>
+                          </div>
+                        </>
+                      )}
+
+                      <div className="flex flex-col text-sm text-neutral-800 font-medium">
+                        P. Compra (und.)
+                        <span className="text-neutral-500 font-normal">{formatToReal(item.costPrice)}</span>
                       </div>
 
-                      <div className="flex flex-col text-sm text-neutral-500">
-                        Estampa
-                        <span className="text-black">{print?.label || 'N/A'}</span>
+                      <div className="flex flex-col text-sm text-neutral-800 font-medium">
+                        P. Venda (und.)
+                        <span className="text-neutral-500 font-normal">{formatToReal(item.salePrice)}</span>
                       </div>
 
-                      <div className="flex flex-col text-sm text-neutral-500">
-                        Cor
-                        <span className="text-black">{color?.label || 'N/A'}</span>
+                      <div className="flex flex-col text-sm text-neutral-800 font-medium">
+                        Lucro (und.)
+                        <span className="text-emerald-600 font-normal">{formatToReal(profit)}</span>
                       </div>
 
-                      <div className="flex flex-col text-sm text-neutral-500">
-                        P. de compra
-                        <span className="text-black">{formatToReal(item.costPrice)}</span>
-                      </div>
-
-                      <div className="flex flex-col text-sm text-neutral-500">
-                        P. de venda
-                        <span className="text-black">{formatToReal(item.salePrice)}</span>
+                      <div className="flex flex-col text-sm text-neutral-800 font-medium">
+                        Lucro total
+                        <span className="text-emerald-600 font-normal">
+                          {formatToReal(profit * item.quantity)} ({getPercentage(profit, item.salePrice)}%)
+                        </span>
                       </div>
                     </div>
                   </div>
